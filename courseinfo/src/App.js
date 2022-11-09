e5fdbd899872b16b72dceee1c51e0d289bcee5bb
Francisco Dimmer <ftdimmer@gmail.com>
2022-11-08 22:36:37 -0300

    exercise 1.2 complete

--- courseinfo/src/App.js
+++ courseinfo/src/App.js
@@ -13,25 +13,35 @@ const App = () => {
     )
   }
 
-  const Content = (props) => {
+  const Part = (props) => {
     return (
-      <p>{props.part} {props.exercises}</p>
+    <p>{props.part} {props.exercises}</p>
     )
   }
 
+  const Content = () => {
+    return (
+      <div>
+        <Part part={part1} exercises={exercises1}/>
+        <Part part={part2} exercises={exercises2}/>
+        <Part part={part3} exercises={exercises3}/>
+      </div>
+    )
+  }
+
+
+
   const Total = (props) => {
     return (
-      <p>Number of exercises {props.exercises}</p>
+      <p>Number of exercises {props.total}</p>
     )
   }
 
   return (
     <div>
       <Header course={course}/>
-      <Content part={part1} exercises={exercises1} />
-      <Content part={part2} exercises={exercises2} />
-      <Content part={part3} exercises={exercises3} />
-      <Total exercises={exercises1 + exercises2 + exercises3}/>
+      <Content />
+      <Total total={exercises1 + exercises2 + exercises3}/>
     </div>
   )
 }
